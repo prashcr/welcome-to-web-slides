65302a9ed8f62c2ca252d73d5eb77d4187e52e85
Prashanth Chandra <coolshanth94@gmail.com>
2016-04-29 22:13:39 +0800

    Add web info

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss', 'css/overrides.css'],
 				tasks: 'css-core'
 			},
 			html: {
